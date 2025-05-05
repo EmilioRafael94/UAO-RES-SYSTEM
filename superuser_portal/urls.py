966e40f5e0fe2951035c2f54ef0b74b035ba4c3c
Emilio Rafael A. Rubio <20220024259@my.xu.edu.ph>
2025-05-05 20:24:49 +0800

    UPDATED SUPERUSER NAVBARS

--- superuser_portal/urls.py
+++ superuser_portal/urls.py
@@ -6,4 +6,8 @@ app_name = 'superuser_portal'
 urlpatterns = [
     path('dashboard/', views.superuser_dashboard, name='superuser_dashboard'),
     path('finalize_reservation/<int:pk>/', views.superuser_finalize_reservation, name='finalize_reservation'),
+    path('manage-reservations/', views.manage_reservations, name='manage_reservations'),
+    path('user-roles/', views.user_roles, name='user_roles'),
+    path('system-settings/', views.system_settings, name='system_settings'),
+
 ]
--- superuser_portal/views.py
+++ superuser_portal/views.py
@@ -3,18 +3,47 @@ from reservations.views import finalize_reservation
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from reservations.models import Reservation
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import user_passes_test
+
+def is_superuser(user):
+    return user.is_superuser
 @login_required
+@user_passes_test(is_superuser)
+def system_settings(request):
+    return render(request, 'superuser/superuser_systemsettings.html')
+
+def is_superuser(user):
+    return user.is_superuser
+
+@login_required
+@user_passes_test(is_superuser)
+def user_roles(request):
+    users = User.objects.all()
+    return render(request, 'superuser/superuser_userroles.html', {'users': users})
+
+# Optional: Limit to superusers
+def is_superuser(user):
+    return user.is_superuser
+
+@login_required
+@user_passes_test(is_superuser)
+def manage_reservations(request):
+    return render(request, 'superuser/superuser_managereservations.html')
+
+@login_required
+@user_passes_test(is_superuser)
 def superuser_dashboard(request):
-    # Fetch admins (users who are staff but not superusers)
     admins = User.objects.filter(is_staff=True).exclude(is_superuser=True)
-    
-    # Fetch all reservations
     reservations = Reservation.objects.all()
 
-    return render(request, 'superuser_dashboard.html', {
+    return render(request, 'superuser/superuser_dashboard.html', {
         'admins': admins,
         'reservations': reservations
     })
+
 def superuser_finalize_reservation(request, pk):
     return finalize_reservation(request, pk)
 
